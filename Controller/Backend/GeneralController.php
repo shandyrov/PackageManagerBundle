b52689aec7282cb94f10acc234824fe33dd0cd2b
shandyrov <shandyrov@gmail.com>
2019-04-08 22:21:19 +0500

    Create GeneralController.php

---
+++ Controller/Backend/GeneralController.php
@@ -0,0 +1,26 @@
+<?php
+namespace Sputnik\PackageManagerBundle\Controller\Backend;
+
+use Sputnik\TemplateBundle\Service\TemplateManager;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class GeneralController extends AbstractController
+{
+    /**
+     * @var $templateManager TemplateManager
+     */
+    private $templateManager;
+
+    public function __construct(TemplateManager $templateManager)
+    {
+        $this->$templateManager = $templateManager;
+    }
+
+    public function index(Response $response)
+    {
+
+        return $this->render($this->templateManager->getCurrentBackend().'/general/index.html.twig');
+    }
+
+}
